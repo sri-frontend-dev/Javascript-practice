520d8543579cde731c748754ff12412230a9a58d
sri-frontend-dev <sridhar.frontend@gmail.com>
2026-02-09 19:52:23 +0530

    add javascript basics practice

---
+++ Day 1- JS Basics – variables/script.js
@@ -0,0 +1,140 @@
+/* 1. variables */
+
+let code = 30;
+code = 50;
+
+console.log(code);
+
+
+const myage = 18;
+
+console.log(myage);
+
+
+const user = {name: "sri"};
+user.name = "frontend Dev";
+
+console.log(user.name);
+
+/* task */
+
+const name = "Sridhar";
+
+console.log(name);
+
+/*  */
+let a = 10;
+let b = 20;
+
+let sum = a + b;
+let difference = b - a;
+let product = a * b;
+
+console.log(sum, difference,product);
+
+/* ------ */
+ let age = 25;
+ age = 26;
+ console.log(age);  // let → reassign allowed
+ 
+
+const age2 = 25;
+age = 25;
+console.log(age2);   // const → reassign NOT allowed
+
+/*  */
+
+let city = "chennai";
+
+console.log(`I live in ${city}`);
+
+/*  */
+
+let price = 1000;
+let discount = 10;
+
+let finalprice = price - (price * discount / 100);
+console.log(`Final price is ${finalprice}`);
+
+
+console.log(x);
+var x = 5;
+
+
+let count  = 0;
+
+count +=1;
+
+console.log(count);
+
+count +=1;
+
+console.log(count);
+
+
+/* Data Types */
+
+let x = 10;
+let c = x;
+c = 20;
+
+console.log(x);
+
+let z  = { value: 10};
+let h = a;
+h.value = 20;
+
+console.log(a);
+
+
+let  ab = 10;
+let  ba = "number";
+let  num =  true;
+console.log( typeof ab);
+console.log( typeof ba);
+
+
+let x = "10";
+let y = 5;
+console.log(x + y);
+console.log(Number(x) + y);
+
+
+let ac;
+let bc = null;
+
+console.log(ac);  //undefined
+console.log(typeof ac); //undefined
+console.log(bc); //null
+console.log(typeof bc);   // object
+
+
+
+let ax = 10;
+let bx = a;
+b = 20;
+
+let arr1 = [1,2];
+let arr2 = arr1;
+arr2.push(3);
+
+console.log( ax,bx);
+console.log(arr1, arr2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
